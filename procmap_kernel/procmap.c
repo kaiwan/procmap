6eabbcf97b63d3b2446502686aa3c23c300ae9ae
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2020-08-05 10:28:41 +0530

    cleanup; flawfinder: s/strncat/strlcat

--- procmap_kernel/procmap.c
+++ procmap_kernel/procmap.c
@@ -3,14 +3,14 @@
  ***************************************************************
  * Brief Description:
  * This kernel module forms the kernel component of the 'procmap' project.
- * 
+ *
  * The procmap project's intention is, given a process's PID, it will display
  * (in a CLI/console output format only for now) a complete 'memory map' of the
- * process VAS (virtual address space). 
- * 
+ * process VAS (virtual address space).
+ *
  * Note:- BSD has a utility by the same name: procmap(1), this project isn't
  * the same, though (quite obviously) some aspects are similar.
- * 
+ *
  * Works on both 32 and 64-bit systems of differing architectures (note: only
  * lightly tested on ARM and x86 32 and 64-bit systems).
  ***************************************************************
@@ -33,22 +33,22 @@
 #define OURMODNAME   "procmap"
 
 MODULE_AUTHOR("Kaiwan N Billimoria");
-MODULE_DESCRIPTION(
-	"procmap: an LKM, the kernel component of the procmap project");
+MODULE_DESCRIPTION("procmap: an LKM, the kernel component of the procmap project");
 MODULE_LICENSE("Dual MIT/GPL");
 MODULE_VERSION("0.1");
 
 // For portability between 32 and 64-bit platforms
-#if(BITS_PER_LONG == 32)
-	#define FMTSPC		"%08x"
-	#define FMTSPC_DEC	"%7d"
-	#define TYPECST		unsigned int
+#if (BITS_PER_LONG == 32)
+#define FMTSPC		"%08x"
+#define FMTSPC_DEC	"%7d"
+#define TYPECST		unsigned int
 #elif(BITS_PER_LONG == 64)
-	#define FMTSPC		"%016lx"
-	#define FMTSPC_DEC	"%9ld"
-	#define TYPECST	    unsigned long
+#define FMTSPC		"%016lx"
+#define FMTSPC_DEC	"%9ld"
+#define TYPECST	    unsigned long
 #endif
 
+#define MAXLEN		2048
 #define ELLPS "|                           [ . . . ]                         |\n"
 
 static struct dentry *gparent;
@@ -56,7 +56,7 @@ static struct dentry *gparent;
 /* We use a mutex lock; details in Ch 15 and Ch 16 */
 DEFINE_MUTEX(mtx);
 
-/* 
+/*
  * query_kernelseg_details
  * Display kernel segment details as applicable to the architecture we're
  * currently running upon.
@@ -85,9 +85,9 @@ static void query_kernelseg_details(char *buf)
 #ifdef ARM
 	memset(tmpbuf, 0, TMPMAX);
 	snprintf(tmpbuf, TMPMAX,
-	FMTSPC "," FMTSPC ",r--,vector table\n",
-	(TYPECST)VECTORS_BASE, (TYPECST)VECTORS_BASE+PAGE_SIZE);
-	strncat(buf, tmpbuf, TMPMAX-1);
+		 FMTSPC "," FMTSPC ",r--,vector table\n",
+		 (TYPECST) VECTORS_BASE, (TYPECST) VECTORS_BASE + PAGE_SIZE);
+	strlcat(buf, tmpbuf, MAXLEN);
 #endif
 
 	/* kernel fixmap region */
@@ -101,15 +101,16 @@ static void query_kernelseg_details(char *buf)
 	 */
 #define FIXADDR_START   0xffc00000UL
 #define FIXADDR_END     0xfff00000UL
-                //SHOW_DELTA_M((TYPECST)FIXADDR_START, (TYPECST)FIXADDR_END));
+	//SHOW_DELTA_M((TYPECST)FIXADDR_START, (TYPECST)FIXADDR_END));
 #else
 #include <asm/fixmap.h>
-	 // seems to work fine on x86
+	// seems to work fine on x86
 	memset(tmpbuf, 0, TMPMAX);
 	snprintf(tmpbuf, TMPMAX,
-		FMTSPC "," FMTSPC ",r--,fixmap region\n",
-		(TYPECST)FIXADDR_START, (TYPECST)FIXADDR_START+FIXADDR_SIZE);
-	strncat(buf, tmpbuf, TMPMAX-1);
+		 FMTSPC "," FMTSPC ",r--,fixmap region\n",
+		 (TYPECST) FIXADDR_START, (TYPECST) FIXADDR_START + FIXADDR_SIZE);
+	//strncat(buf, tmpbuf, TMPMAX - 1);
+	strlcat(buf, tmpbuf, MAXLEN);
 #endif
 
 	/* kernel module region
@@ -118,43 +119,48 @@ static void query_kernelseg_details(char *buf)
 	 * ARM-32); so we rearrange the order in which it's shown depending on the
 	 * arch, thus trying to maintain a 'by descending address' ordering.
 	 */
-#if(BITS_PER_LONG == 64)
+#if (BITS_PER_LONG == 64)
 	memset(tmpbuf, 0, TMPMAX);
 	snprintf(tmpbuf, TMPMAX,
-		FMTSPC "," FMTSPC ",rwx,module region\n",
-		(TYPECST)MODULES_VADDR, (TYPECST)MODULES_END);
-	strncat(buf, tmpbuf, TMPMAX-1);
+		 FMTSPC "," FMTSPC ",rwx,module region\n",
+		 (TYPECST) MODULES_VADDR, (TYPECST) MODULES_END);
+	//strncat(buf, tmpbuf, TMPMAX - 1);
+	strlcat(buf, tmpbuf, MAXLEN);
 #endif
 
-#ifdef CONFIG_KASAN  // KASAN region: Kernel Address SANitizer
+#ifdef CONFIG_KASAN		// KASAN region: Kernel Address SANitizer
 	memset(tmpbuf, 0, TMPMAX);
 	snprintf(tmpbuf, TMPMAX,
-	FMTSPC "," FMTSPC ",rw-,KASAN shadow\n",
-	(TYPECST)KASAN_SHADOW_START, (TYPECST)KASAN_SHADOW_END);
-	strncat(buf, tmpbuf, TMPMAX-1);
+		 FMTSPC "," FMTSPC ",rw-,KASAN shadow\n",
+		 (TYPECST) KASAN_SHADOW_START, (TYPECST) KASAN_SHADOW_END);
+	//strncat(buf, tmpbuf, TMPMAX - 1);
+	strlcat(buf, tmpbuf, MAXLEN);
 #endif
 
 	/* vmalloc region */
 	memset(tmpbuf, 0, TMPMAX);
 	snprintf(tmpbuf, TMPMAX,
-		FMTSPC "," FMTSPC ",rw-,vmalloc region\n",
-		(TYPECST)VMALLOC_START, (TYPECST)VMALLOC_END);
-	strncat(buf, tmpbuf, TMPMAX-1);
+		 FMTSPC "," FMTSPC ",rw-,vmalloc region\n",
+		 (TYPECST) VMALLOC_START, (TYPECST) VMALLOC_END);
+	//strncat(buf, tmpbuf, TMPMAX - 1);
+	strlcat(buf, tmpbuf, MAXLEN);
 
 	/* lowmem region: spans from PAGE_OFFSET to high_memory */
 	memset(tmpbuf, 0, TMPMAX);
 	snprintf(tmpbuf, TMPMAX,
-		FMTSPC "," FMTSPC ",rwx,lowmem region\n",
-		(TYPECST)PAGE_OFFSET, (TYPECST)high_memory);
-	strncat(buf, tmpbuf, TMPMAX-1);
+		 FMTSPC "," FMTSPC ",rwx,lowmem region\n",
+		 (TYPECST) PAGE_OFFSET, (TYPECST) high_memory);
+	//strncat(buf, tmpbuf, TMPMAX - 1);
+	strlcat(buf, tmpbuf, MAXLEN);
 
 	/* (possible) highmem region;  may be present on some 32-bit systems */
 #ifdef CONFIG_HIGHMEM
 	memset(tmpbuf, 0, TMPMAX);
 	snprintf(tmpbuf, TMPMAX,
-	FMTSPC "," FMTSPC ",rwx,HIGHMEM region\n",
-	(TYPECST)PKMAP_BASE, (TYPECST)(PKMAP_BASE)+(LAST_PKMAP*PAGE_SIZE));
-	strncat(buf, tmpbuf, TMPMAX-1);
+		 FMTSPC "," FMTSPC ",rwx,HIGHMEM region\n",
+		 (TYPECST) PKMAP_BASE, (TYPECST) (PKMAP_BASE) + (LAST_PKMAP * PAGE_SIZE));
+	//strncat(buf, tmpbuf, TMPMAX - 1);
+	strlcat(buf, tmpbuf, MAXLEN);
 #endif
 
 	/*
@@ -167,29 +173,29 @@ static void query_kernelseg_details(char *buf)
 	 * for modules; thus we don't attempt to print them.
 	 */
 
-#if(BITS_PER_LONG == 32)  /* modules region: see the comment above reg this */
+#if (BITS_PER_LONG == 32)	/* modules region: see the comment above reg this */
 	memset(tmpbuf, 0, TMPMAX);
 	snprintf(tmpbuf, TMPMAX,
-		FMTSPC "," FMTSPC ",rwx,module region:\n",
-		(TYPECST)MODULES_VADDR, (TYPECST)MODULES_END);
-	strncat(buf, tmpbuf, TMPMAX-1);
+		 FMTSPC "," FMTSPC ",rwx,module region:\n",
+		 (TYPECST) MODULES_VADDR, (TYPECST) MODULES_END);
+	//strncat(buf, tmpbuf, TMPMAX - 1);
+	strlcat(buf, tmpbuf, MAXLEN);
 #endif
 
 #include <asm/processor.h>
 	/* Enhancement: also pass along other key kernel vars */
 	memset(tmpbuf, 0, TMPMAX);
 	snprintf(tmpbuf, TMPMAX,
-		"PAGE_SIZE," FMTSPC "\n"
-		"TASK_SIZE," FMTSPC "\n",
-		(TYPECST)PAGE_SIZE, (TYPECST)TASK_SIZE);
-	strncat(buf, tmpbuf, TMPMAX-1);
+		 "PAGE_SIZE," FMTSPC "\n"
+		 "TASK_SIZE," FMTSPC "\n", (TYPECST) PAGE_SIZE, (TYPECST) TASK_SIZE);
+	//strncat(buf, tmpbuf, TMPMAX - 1);
+	strlcat(buf, tmpbuf, MAXLEN);
 }
 
 /* Our debugfs file 1's read callback function */
 static ssize_t dbgfs_show_kernelseg(struct file *filp, char __user *ubuf,
-				 size_t count, loff_t *fpos)
+				    size_t count, loff_t *fpos)
 {
-#define MAXLEN 2048
 	char *kbuf;
 	ssize_t ret = 0;
 
@@ -204,8 +210,7 @@ static ssize_t dbgfs_show_kernelseg(struct file *filp, char __user *ubuf,
 
 	query_kernelseg_details(kbuf);
 
-	ret = simple_read_from_buffer(ubuf, MAXLEN, fpos, kbuf,
-				       strlen(kbuf));
+	ret = simple_read_from_buffer(ubuf, MAXLEN, fpos, kbuf, strlen(kbuf));
 	//MSG("ret = %ld\n", ret);
 	kfree(kbuf);
 	mutex_unlock(&mtx);
@@ -227,35 +232,30 @@ static int setup_debugfs_file(void)
 		return -EINVAL;
 	}
 
-	/* Create a dir under the debugfs mount point, whose name is the
-	 * module name */
+	/* Create a dir under the debugfs mount point, whose name is the module name */
 	gparent = debugfs_create_dir(OURMODNAME, NULL);
 	if (!gparent) {
-		pr_info("%s: debugfs_create_dir failed, aborting...\n",
-			OURMODNAME);
+		pr_info("%s: debugfs_create_dir failed, aborting...\n", OURMODNAME);
 		stat = PTR_ERR(gparent);
 		goto out_fail_1;
 	}
 
 	/* Create a generic debugfs file */
 #define DBGFS_FILE1	"disp_kernelseg_details"
-	file1 =
-	    debugfs_create_file(DBGFS_FILE1, 0444, gparent, (void *)NULL,
-				&dbgfs_fops);
+	file1 = debugfs_create_file(DBGFS_FILE1, 0444, gparent, (void *)NULL, &dbgfs_fops);
 	if (!file1) {
-		pr_info("%s: debugfs_create_file failed, aborting...\n",
-			OURMODNAME);
+		pr_info("%s: debugfs_create_file failed, aborting...\n", OURMODNAME);
 		stat = PTR_ERR(file1);
 		goto out_fail_2;
 	}
 	pr_debug("%s: debugfs file 1 <debugfs_mountpt>/%s/%s created\n",
 		 OURMODNAME, OURMODNAME, DBGFS_FILE1);
 
-	return 0;	/* success */
+	return 0;		/* success */
 
-out_fail_2:
+ out_fail_2:
 	debugfs_remove_recursive(gparent);
-out_fail_1:
+ out_fail_1:
 	return stat;
 }
 
