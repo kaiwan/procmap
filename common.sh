b1fe0feb93b0700bdbaa6f7ff79e7220260224a5
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2023-01-12 16:06:46 +0530

    Add yad to -warning- apps to install

--- common.sh
+++ common.sh
@@ -354,7 +354,7 @@ verify_utils_present()
 {
 check_deps_fatal "getconf bc make gcc kmod grep awk sed kill readlink head tail \
 cut cat tac sort wc ldd file"
-check_deps_warn "sudo tput ps smem"
+check_deps_warn "sudo tput ps smem yad"
 [ ! -d /proc ] && FatalError "proc fs not available or not mounted? Aborting..." || true
 }
 
